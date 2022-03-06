41dacd3d2926f1293ca895b4b7026bbe86fdd8b9
opendisk <95396908+opendisk@users.noreply.github.com>
2022-03-07 02:55:10 +0700

    Update Ask.php

--- src/Ask.php
+++ src/Ask.php
@@ -2,7 +2,7 @@
 
 namespace Opendisk\SentenceFinder;
 
-class Ask extends SentenceFinder
+class Ask extends GetSentence
 {
     public $base_url = "http://ask.com/web?q=";
     public $selector = ".PartialSearchResults-item .PartialSearchResults-item-abstract";
