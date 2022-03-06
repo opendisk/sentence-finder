ead489d4555eee2e205abd1ad4cddd72668778d8
opendisk <95396908+opendisk@users.noreply.github.com>
2022-03-07 02:54:50 +0700

    Update Bing.php

--- src/Bing.php
+++ src/Bing.php
@@ -2,7 +2,7 @@
 
 namespace Opendisk\SentenceFinder;
 
-class Bing extends SentenceFinder
+class Bing extends GetSentence
 {
     public $base_url = "https://www.bing.com/search?q=";
     public $selector = "li.b_algo .b_caption p";
